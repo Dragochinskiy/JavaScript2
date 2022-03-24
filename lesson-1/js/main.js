06586b59d75baeb199bd87bcd55c5d8cf735819c
Dmitriy Dragochinskiy <dmitriy.dragochinskiy@mail.ru>
2022-03-24 08:46:13 +0300

    Work on bugs

--- lesson-1/js/main.js
+++ lesson-1/js/main.js
@@ -2,23 +2,21 @@ const products = [
   { id: 1, title: 'Notebook', price: 1000, img: 'http://via.placeholder.com/650x360' },
   { id: 2, title: 'Mouse', price: 100 },
   { id: 3, title: 'Keyboard', price: 250 },
-  { id: 4, title: 'Gamepad', price: 150 },
+  { id: 4, title: 'Gamepad', price: 150 }
 ];
 
-const renderProduct = (title = 'Error title', price = 'Error price', img = 'http://via.placeholder.com/640x360') => {
+const renderProduct = (product, img = 'http://via.placeholder.com/640x360') => {
   return `<div class="product-item">
-            <h3>${title}</h3>
+            <h3>${product.title}</h3>
             <img src="${img}" alt="">
-            <p>${price}</p>
+            <p>${product.price}</p>
             <button class="by-btn">Добавить</button>
           </div>`;
 };
 
-const renderCatalog = (list) => {
-  const productList = list.map(good => renderProduct(good.title, good.price, good.img));
-  // console.log(productList);
-
-  document.querySelector('.products').innerHTML = productList;
+const renderPage = list => {
+  document.querySelector('.products').innerHTML =
+    list.map(product => renderProduct(product)).join('');
 };
 
-renderCatalog(products);
+renderPage(products);
